4ca1ad7317af789940d6a2864d45924d226355a0
milosz <skorupskimilosz@gmail.com>
2025-12-06 15:45:18 +0100

    dodanie projektu

---
+++ cwiczenia_zmienne.py
@@ -0,0 +1,15 @@
+def dodaj(a, b):
+    print("Jestem w środku funkcji!")
+    return a + b
+
+wynik = dodaj(2, 3)
+print("Wynik:", wynik)
+
+
+
+def odejmij(c,d):
+    print("jestem w srodku funkcji odejmij")
+    return c-d
+
+wynik = odejmij(4, 1)
+print("Wynik:", wynik)
---
+++ deklaracja_inicjalizacja_zmiennych.py
@@ -0,0 +1,44 @@
+from dataclasses import replace
+from operator import truediv
+
+liczba1 = 1
+liczba2 = 2.5
+liczba3 = "wiesiek"
+liczba4 = 'zdzisiek'
+liczba5 = True
+liczba6 = False
+
+print(type(liczba1))
+print(type(liczba2))
+print(type(liczba3))
+print(type(liczba4))
+print(type(liczba5))
+print(type(liczba6))
+
+# wynik = liczba1 + liczba2 + liczba3 + liczba4 + liczba5 + liczba6
+
+
+# taka operacja jest nie dozowolona, tylko na tym samym typie jest dozwolona
+# print(liczba1.replace(1,3)) nie nizba zamienic 1 z wieskiem
+
+print(liczba1 + liczba2)
+
+# Python pozwala dodawać tylko typy,
+# które mają zdefiniowaną wspólną operację + (np. liczby między sobą, stringi między sobą).
+# Mieszanie typów jak int + str nie jest możliwe bez konwersji.
+
+#konwersja strina na liczbe
+wiek = 20
+print("Mam " + str(wiek) + " lat")
+
+# przy pomocy f-stringów:
+wiek = 20
+print(f"Mam {wiek} lat")
+
+#konwersja liczby na string
+liczba = 123
+tekst = str(liczba)
+
+print(tekst)        # "123"
+print(type(tekst))  # <class 'str'>
+
