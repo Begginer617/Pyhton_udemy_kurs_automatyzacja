69535ecfc7af4bc8d2e7f743a32cf6c71ddb3a01
milosz <skorupskimilosz@gmail.com>
2025-12-06 19:18:26 +0100

    dodanie przykładu

--- cwiczenia_zmienne.py
+++ cwiczenia_zmienne.py
@@ -13,3 +13,26 @@ def odejmij(c,d):
 
 wynik = odejmij(4, 1)
 print("Wynik:", wynik)
+
+
+
+#bez return
+def iloczyn(c,d):
+    print("jestem w srodku funkcji iloczyn")
+    #return c*d
+
+    print(iloczyn())
+
+#wynik = iloczyn(4, 1)
+#print("Wynik:", wynik)
+
+
+#Przykład, który idealnie pokazuje różnicę miedzy return a print
+def pomnoz(a, b):
+    #print() w środku funkcji — wyświetla 8
+    print(a * b)
+    #return — zwraca 8 do zmiennej x
+    return a * b
+
+x = pomnoz(2, 4)
+print("x to:", x)
