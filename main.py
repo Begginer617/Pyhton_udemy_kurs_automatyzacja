66ee966d397e58c942b3bb4c3c62c1d15728b53c
Begginer617 <skorupskimilosz@gmail.com>
2025-12-08 09:58:59 +0000

    Work in progress

---
+++ main.py
@@ -0,0 +1,11 @@
+napis = "ala ma kota"
+print(napis)
+
+napis.count('a') # liczy powtórzenia
+print(napis.count('a'))
+napis.count("kota")
+print(napis.count("kota"))
+napis.capitalize()
+print(napis.capitalize())
+napis.upper()
+print(napis)
